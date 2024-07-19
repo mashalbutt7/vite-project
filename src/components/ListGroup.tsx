4c10de65ca63f019755f00e2e83ab37656561789
Mashal Pervaiz <mashalbutt1028@gmail.com>
2024-07-19 23:18:26 +0500

    condition rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,11 @@
 import { Fragment } from "react/jsx-runtime";
 function ListGroup() {
   const items = ["Lahore", "Karachi", "Quetta", "Islamabad", "Multan"]; //method map
-
   return (
     <Fragment>
       <h1>List</h1>
+
+      {items.length === 0 && <p>no item found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
@@ -16,3 +17,5 @@ function ListGroup() {
 export default ListGroup;
 // a componenet cant return more than one element (use div or fragment or empty angle brackets which tells react to add a fragment)
 //key to track items in case we add or delete items
+//braces exception in jsx elements only html element or react comp
+// also use a function for this or a const diff msg can be passed if we use a fucntion
